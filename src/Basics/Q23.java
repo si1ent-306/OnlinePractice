d62e3b5063390c7b5dc8d943b1f6f2841c7ef16e
wagner3956 <elliottjwag@hotmail.com>
2024-10-17 13:23:27 -0600

    Q23

---
+++ src/Basics/Q23.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Q23 {
+    public static void main(String[] args) {
+        int asciiCode = 3486; // Replace this with the desired ASCII code
+        char character = (char) asciiCode;
+        System.out.println(character);
+    }
+}
